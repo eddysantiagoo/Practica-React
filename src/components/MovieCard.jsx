06edc9a4b4bd1bd3857c2890931958de7809ea05
eddysantiagoo <eddysantiagogh@gmail.com>
2022-05-02 14:42:00 -0500

    Agregar estilos al componente MovieGrid para organizar la visualización de las imagenes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,10 @@ export function MovieCard({ movie }) {
     const imagenURL = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
     return (
         <li className={styles.movieCard}>
-            <img className={styles.movieImage}src={imagenURL} alt={movie.title} />
+            <img 
+            width={230}
+            height={345}
+            className={styles.movieImage}src={imagenURL} alt={movie.title} />
             <div>{movie.title}</div>
         </li>
     );
--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,9 +1,10 @@
 import movies from "../movies"
 import { MovieCard } from "./MovieCard";
+import styles from "../MovieGrid.module.css"
 
 export function MoviesGrid() {
     return (
-        <ul>
+        <ul className={styles.moviesGrid}>
             {movies.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
             ))}
