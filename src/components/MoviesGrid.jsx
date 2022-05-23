fe386c746bafd6bf8f8d1abb0777fa0e811760a4
eddysantiagoo <eddysantiagogh@gmail.com>
2022-05-23 10:44:39 -0500

    Agregar conexion con API usando hooks para mostrar lista de peliculas en el landing

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,8 +1,26 @@
 import movies from "../movies"
 import { MovieCard } from "./MovieCard";
 import styles from "../MovieGrid.module.css"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export function MoviesGrid() {
+    // let movies = [];
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/discover/movie", {
+            headers:  {
+                Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZWI1Mjc2MWQzYzVmYjcwZDg0Y2FhNjhjNzU1OWYwNiIsInN1YiI6IjYyODdjMTEwMWYzZTYwMDA1MmU3Y2MyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VUqObJHLiZxHcmFmD4tpbxtM0V8tFC5DdsXpe9A_vko",
+                "Content-Type": "application/json;charset=utf-8",
+            },
+        }).then((result) => result.json())
+          .then((data) => {
+            setMovies(data.results);
+        });
+    }, []);
+
     return (
         <ul className={styles.moviesGrid}>
             {movies.map((movie) => (
