0e4797144d2e46f68effcf067353c46a091556f0
eddysantiagoo <eddysantiagogh@gmail.com>
2022-05-23 21:27:54 -0500

    Agregar hook en MovieDetails para ver los detalles de cada pelicula

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,25 @@
 import styles from "../MovieDetails.module.css"
 import movie from "../movie.json"
 import { useParams } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
+import { get } from "../utils/htttpClient";
+import { useState } from "react";
 
 
 export function MovieDetails(){
 
     const { movieId } = useParams();
-    console.log(movieId)
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        get("/movie/" + movieId).then(data => {
+            setMovie(data);
+        })
+    }, [movieId]);
+
+    if(!movie){
+        return null;
+    }
 
     const imagenURL = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
     return <div className={styles.detailsContainer}>
@@ -14,12 +27,12 @@ export function MovieDetails(){
         <img className={`${styles.col} ${styles.movieImage}`} src={imagenURL} alt={movie.title} />
         <div className={`${styles.col} ${styles.movieDetails}`}>
             <p className={styles.firstItem}>
-             <strong>Title:</strong>{movie.title}
+             <strong>Title: </strong>{movie.title}
             </p>
             <p>
-                {movie.genres.map(genres => genres.name)}
+               <strong>Genres: </strong> {movie.genres.map(genres => genres.name + " ")}
             </p>
-            <p><strong>Decription:</strong>{movie.overview}
+            <p><strong>Decription: </strong>{movie.overview}
             </p>
         </div>
 
