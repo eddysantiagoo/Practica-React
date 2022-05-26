817611bedc23ebbcc06f5f05d84cbb039ea15dd7
eddysantiagoo <eddysantiagogh@gmail.com>
2022-05-26 11:12:26 -0500

    Agregar barra de busqueda, unicamente con html y css

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import styles from "../Search.module.css"
+import {FaSearch} from "react-icons/fa"
+
+export default function Search() {
+  return (
+    <form className={styles.searchContainer}>
+        <div className={styles.searchBox}>
+            <input type="text" className={styles.searchInput}/>
+            <button type="submit" className={styles.searchButton}><FaSearch size={15} /></button>
+        </div>
+    </form>
+  )
+}
