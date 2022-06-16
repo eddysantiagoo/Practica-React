083b23d9572f3903f6fbc75cce9cbc8e5aab37a2
eddysantiagoo <eddysantiagogh@gmail.com>
2022-06-16 13:32:30 -0500

    Configurar las variables de entorno, semi F

--- src/utils/htttpClient.js
+++ src/utils/htttpClient.js
@@ -1,10 +1,12 @@
-const API = "https://api.themoviedb.org/3"
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+
 
 export function get(path) {
     return fetch(API + path, {
         headers: {
             Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZWI1Mjc2MWQzYzVmYjcwZDg0Y2FhNjhjNzU1OWYwNiIsInN1YiI6IjYyODdjMTEwMWYzZTYwMDA1MmU3Y2MyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VUqObJHLiZxHcmFmD4tpbxtM0V8tFC5DdsXpe9A_vko",
+                "Bearer " + API_TOKEN,
             "Content-Type": "application/json;charset=utf-8",
         },
     }).then((result) => result.json())
